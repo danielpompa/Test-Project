b255b807d366de23a73d8ab327cfe56689169308
danielpompa <pompadaniel@gmail.com>
2014-11-06 23:52:29 +0200

    f
    f

---
+++ testproject/dist/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){        
+            $('.img-responsive').on('click',function(){
+                var src = $(this).attr('src');
+                var img = '<img src="' + src + '" class="img-responsive"/>';
+                                                                
+                $('#myModal').modal();
+                $('#myModal').on('shown.bs.modal', function(){
+                    $('#myModal .modal-body').html(img);
+                })
+                $('#myModal').on('hidden.bs.modal', function(){
+                    $('#myModal .modal-body').html('');
+                });
+                
+           });
+        
+})  
