d2970cbe726a14d4d9e8a53a943a6954ab58c3e3
danielpompa <pompadaniel@gmail.com>
2014-11-07 09:58:37 +0200

    f

--- testproject/dist/js/main.js
+++ testproject/dist/js/main.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){        
             $('.img-responsive').on('click',function(){
                 var src = $(this).attr('src');
-                var img = '<img src="' + src + '" class="img-responsive"/>';
-                                                                
+				var id = $(this).attr('id');
+				id = id.split("newsimage");
+                var html = '<img src="' + src + '" class="img-responsive"/>';
+				html += $('#newtext' + id[1]).html(); 
+				
                 $('#myModal').modal();
                 $('#myModal').on('shown.bs.modal', function(){
-                    $('#myModal .modal-body').html(img);
+                    $('#myModal .modal-body').html(html);
                 })
                 $('#myModal').on('hidden.bs.modal', function(){
                     $('#myModal .modal-body').html('');
